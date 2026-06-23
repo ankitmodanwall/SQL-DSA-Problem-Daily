1fbeebec208a8fe2240defc89f65e13576f946bc
ANKIT MODANWAL <166202965+ankitmodanwall@users.noreply.github.com>
2026-06-23 22:12:18 +0530

    Time: 16 ms (19.38%), Space: 69.8 MB (18.02%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -12,7 +12,6 @@ class Solution {
         if(root == null || root== p || root == q){
             return root;
         }
-        // TreeNode left = LCA( root, TreeNode left, TreeNode right){
             TreeNode left = lowestCommonAncestor(root.left, p, q);
             TreeNode right = lowestCommonAncestor(root.right, p, q);
             if(left != null && right != null){
