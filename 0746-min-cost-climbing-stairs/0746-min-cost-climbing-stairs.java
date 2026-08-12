3c29b94723d6e4d7c486cfcf68882e5bb14c5676
ANKIT MODANWAL <ankitmodanwal001@gmail.com>
2026-08-12 14:41:12 +0530

    Time: 1 ms (39.72%), Space: 44.9 MB (40.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,12 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n + 1];
-        for (int i = 2; i <= n; i++) {
-            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        int[] a = new int[cost.length+1];
+        Arrays.fill(a, -1);
+        return Math.min(sol(a, cost, 0), sol(a, cost, 1));
+    }
+    int sol (int a[], int cost[], int i){
+        if(i>= cost.length){
+            return 0;
         }
-        return dp[n];
+        if(a[i]!=-1) return a[i];
+        a[i] = cost[i]+Math.min(sol(a, cost, i+1), sol(a, cost, i +2));
+        return a[i];
     }
 }
