30c4c212ba2fa0cd941fe9be62fdc012089b155a
ANKIT MODANWAL <ankitmodanwal001@gmail.com>
2026-09-01 14:29:54 +0530

    Time: 489 ms (66.67%), Space: 269.2 MB (61.9%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.java
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.java
@@ -1,69 +1,43 @@
+import java.util.*;
+
 class Solution {
-    public int minMoves(String[] classroom, int energy) {
-        int m = classroom.length;
-        int n = classroom[0].length();
-        int[][] litterMap = new int[m][n];
-        int startX = -1, startY = -1;
-        int litterCount = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                char c = classroom[i].charAt(j);
-                if (c == 'S') {
-                    startX = i;
-                    startY = j;
-                } else if (c == 'L') {
-                    litterMap[i][j] = litterCount++;
+    public int minMoves(String[] a, int E) {
+        int m=a.length,n=a[0].length(),sr=0,sc=0,k=0;
+        int[][] id=new int[m][n];
+        for(int[] x:id) Arrays.fill(x,-1);
+
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++) {
+                if(a[i].charAt(j)=='S'){sr=i;sc=j;}
+                if(a[i].charAt(j)=='L') id[i][j]=k++;
+            }
+
+        int all=(1<<k)-1;
+        boolean[][][][] v=new boolean[m][n][E+1][1<<k];
+        Queue<int[]> q=new ArrayDeque<>();
+        q.add(new int[]{sr,sc,E,0,0});
+        v[sr][sc][E][0]=true;
+
+        int[] dr={-1,1,0,0},dc={0,0,-1,1};
+
+        while(!q.isEmpty()){
+            int[] x=q.poll();
+            if(x[3]==all) return x[4];
+
+            for(int d=0;d<4;d++){
+                int r=x[0]+dr[d],c=x[1]+dc[d],e=x[2]-1,mask=x[3];
+
+                if(r<0||r>=m||c<0||c>=n||a[r].charAt(c)=='X'||e<0) continue;
+
+                if(a[r].charAt(c)=='R') e=E;
+                if(id[r][c]!=-1) mask|=1<<id[r][c];
+
+                if(!v[r][c][e][mask]){
+                    v[r][c][e][mask]=true;
+                    q.add(new int[]{r,c,e,mask,x[4]+1});
                 }
             }
         }
-        
-        if (litterCount == 0) return 0;
-        
-        int targetMask = (1 << litterCount) - 1;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startX, startY, energy, 0, 0});
-        
-        boolean[][][][] visited = new boolean[m][n][energy + 1][1 << litterCount];
-        visited[startX][startY][energy][0] = true;
-        
-        int[] dirs = {-1, 0, 1, 0, -1};
-        
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int r = curr[0], c = curr[1], e = curr[2], mask = curr[3], steps = curr[4];
-            
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dirs[i];
-                int nc = c + dirs[i + 1];
-                
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n && classroom[nr].charAt(nc) != 'X') {
-                    int nxt_e = e - 1;
-                    int nxt_mask = mask;
-                    char nextCell = classroom[nr].charAt(nc);
-                    
-                    if (nextCell == 'L') {
-                        nxt_mask |= (1 << litterMap[nr][nc]);
-                    }
-                    
-                    if (nxt_mask == targetMask) {
-                        return steps + 1;
-                    }
-                    
-                    if (nextCell == 'R') {
-                        nxt_e = energy;
-                    }
-                    
-                    if (nxt_e == 0 && nextCell != 'R') continue;
-                    
-                    if (!visited[nr][nc][nxt_e][nxt_mask]) {
-                        visited[nr][nc][nxt_e][nxt_mask] = true;
-                        q.offer(new int[]{nr, nc, nxt_e, nxt_mask, steps + 1});
-                    }
-                }
-            }
-        }
-        
         return -1;
     }
 }
