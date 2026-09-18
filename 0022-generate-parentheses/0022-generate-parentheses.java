43174d35b949710e7a29c657b31832a4bcdc7b63
ANKIT MODANWAL <ankitmodanwal001@gmail.com>
2026-09-18 21:33:44 +0530

    Time: 3 ms (14.34%), Space: 44.7 MB (43.89%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+        List<String> ans = new ArrayList<>();
+        solve("", 0, 0, n, ans);
+        return ans;
+
+    }
+
+    private void solve(String curr, int open, int close, int total, List<String> ans) {
+
+        if (curr.length() == 2 * total) {
+            ans.add(curr);
+            return;
+        }
+
+        if (open < total) {
+
+            solve(curr + "(", open + 1, close, total, ans);
+        }
+        if (close < open) {
+            solve(curr + ")", open, close + 1, total, ans);
+        }
+
+    }
+}
+
+// TC -->> 2^n
