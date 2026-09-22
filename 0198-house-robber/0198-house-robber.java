e0ebbca155d2fe5cee0a26bf676a86cf7b99840f
ANKIT MODANWAL <ankitmodanwal001@gmail.com>
2026-09-22 21:37:11 +0530

    Time: 0 ms (100%), Space: 43 MB (19.42%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[] dp;
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return solve(nums, nums.length-1);
+    }
+    int solve(int[] nums, int i ){
+        if(i<0)
+        return 0;
+        if(dp[0] != -1)
+        return dp[i];
+
+        dp[i] = Math.max(
+            solve(nums, i-1),
+            nums[i] + solve(nums, i-2)
+        );
+        return dp[i];
+    }
+}
